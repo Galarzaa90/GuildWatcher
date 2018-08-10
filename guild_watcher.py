2be3df149723bb259fca8e25d6228d046f88922d
Allan Galarza <allan.galarza@gmail.com>
2018-08-09 21:09:54 -0700

    WIP: Implementing tibia.py

--- guild_watcher.py
+++ guild_watcher.py
@@ -1,11 +1,11 @@
 import json
 import logging
 import pickle
-import re
 import time
-import urllib.parse
+from typing import Optional
 
 import requests
+from tibiapy import Character, Guild
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -48,12 +48,10 @@ def get_character(name, tries=5):
         *chars is list that contains other characters in the same account (if not hidden).
         Each list element is dictionary with the keys: name, world.
     May return ERROR_DOESNTEXIST or ERROR_NETWORK accordingly."""
-    url_character = "https://secure.tibia.com/community/?subtopic=characters&name="
     try:
-        url = url_character + urllib.parse.quote(name.encode('iso-8859-1'))
+        url = Character.get_url(name)
     except UnicodeEncodeError:
         return None
-    char = dict()
 
     # Fetch website
     try:
@@ -61,94 +59,26 @@ def get_character(name, tries=5):
         content = r.text
     except requests.RequestException:
         if tries == 0:
-            return {"error": "Network"}
+            return None
         else:
             tries -= 1
             return get_character(name, tries)
+    char = Character.from_content(content)
+    return char
 
-    # Trimming content to reduce load
+
+def get_guild(name, tries=5) -> Optional[Guild]:
     try:
-        startIndex = content.index('<div class="BoxContent"')
-        endIndex = content.index("<B>Search Character</B>")
-        content = content[startIndex:endIndex]
-    except ValueError:
-        # Website fetch was incomplete, due to a network error
+        r = requests.get(Guild.get_url(name))
+        content = r.text
+    except requests.RequestException:
         if tries == 0:
             return None
         else:
             tries -= 1
-            time.sleep(2)
-            return get_character(name, tries)
-    # Check if player exists
-    if "Name:</td><td>" not in content:
-        return None
+            return get_guild(name, tries)
 
-    # Name
-    m = re.search(r'Name:</td><td>([^<,]+)', content)
-    if m:
-        char['name'] = m.group(1).strip()
-
-    # Vocation
-    m = re.search(r'Vocation:</td><td>([^<]+)', content)
-    if m:
-        char['vocation'] = m.group(1)
-
-    # World
-    m = re.search(r'World:</td><td>([^<]+)', content)
-    if m:
-        char['world'] = m.group(1)
-
-    return char
-
-
-def get_guild_info(name, tries=5):
-    try:
-        r = requests.get("https://secure.tibia.com/community/", params={"subtopic":"guilds","page":"view","GuildName":name})
-        content = r.text
-    except requests.RequestException:
-        if tries == 0:
-            return {"error": "Network"}
-        else:
-            tries -= 1
-            return get_guild_info(name, tries)
-
-    try:
-        start_index = content.index('<div class="BoxContent"')
-        end_index = content.index('<div id="ThemeboxesColumn" >')
-        content = content[start_index:end_index]
-    except ValueError:
-        # Website fetch was incomplete, due to a network error
-        return {"error": "Network"}
-
-    if '<div class="Text" >Error</div>' in content:
-        return {"error": "NotFound"}
-
-    guild = {}
-    # Logo URL
-    m = re.search(r'<IMG SRC=\"([^\"]+)\" W', content)
-    if m:
-        guild['logo_url'] = m.group(1)
-
-        # Regex pattern to fetch members
-        regex_members = r'<TR BGCOLOR=#[\dABCDEF]+><TD>(.+?)</TD>\s</td><TD><A HREF="https://secure.tibia.com/community/\?subtopic=characters&name=(.+?)">.+?</A> *\(*(.*?)\)*</TD>\s<TD>(.+?)</TD>\s<TD>(.+?)</TD>\s<TD>(.+?)</TD>'
-        pattern = re.compile(regex_members, re.MULTILINE + re.S)
-
-        m = re.findall(pattern, content)
-        guild['members'] = []
-        # Check if list is empty
-        if m:
-            # Building dictionary list from members
-            last_rank = ""
-            guild["ranks"] = []
-            for (rank, name, title, vocation, level, joined) in m:
-                rank = last_rank if (rank == '&#160;') else rank
-                if rank not in guild["ranks"]:
-                    guild["ranks"].append(rank)
-                last_rank = rank
-                name = requests.utils.unquote(name).replace("+", " ")
-                joined = joined.replace('&#160;', '-')
-                guild['members'].append({'rank': rank, 'name': name, 'title': title, 'vocation': vocation,
-                                         'level': level, 'joined': joined})
+    guild = Guild.from_content(content)
 
     return guild
 
@@ -277,8 +207,7 @@ def announce_changes(guild_config, name, changes, joined, total):
     requests.post(guild.get("webhook_url", guild_config.get("webhook_url", cfg.get("webhook_url"))),
                   data=json.dumps(body), headers={"Content-Type": "application/json"})
 
-
-def main():
+if __name__ == "__main__":
     while True:
         # Iterate each guild
         for guild in cfg["guilds"]:
@@ -381,5 +310,99 @@ def main():
         time.sleep(5*60)
 
 
+def scan_guilds():
+    while True:
+        # Iterate through each guild in the configuration file
+        for guild in cfg["guilds"]:
+            if guild.get("webhook_url", cfg.get("webhook_url")) is None:
+                log.error("Missing Webhook URL in config.json")
+                exit()
+            name = guild.get("name")
+            if name is None:
+                log.error("Guild is missing name.")
+                time.sleep(5)
+                continue
+            guild_file = name+".data"
+            guild_data: Guild = load_data(guild_file)
+            if guild_data is None:
+                log.info(name + " - No previous data found. Saving current data.")
+                guild_data = get_guild(name)
+                if guild_data is None:
+                    log.error(name + " - Error: Guild doesn't exist")
+                    continue
+                save_data(guild_file, guild_data)
+                time.sleep(5)
+                continue
+
+            log.info(name + "- Scanning guild...")
+            new_guild_data = get_guild(name)
+            if new_guild_data is None:
+                log.error(name + " - Error: Guild doesn't exist")
+                continue
+            save_data(guild_file, new_guild_data)
+            changes = []
+            # Looping through members
+            total_members = new_guild_data.member_count
+            for member in guild_data.members:
+                found = False
+                # Looping current members
+                for _member in new_guild_data.members:
+                    if member.name == _member.name:
+                        # Member still in guild, we remove it from list for faster iterating
+                        new_guild_data.members.remove(_member)
+                        found = True
+                        # Rank changed
+                        if member.rank != _member.rank:
+                            try:
+                                if new_guild_data.ranks.index(member.rank) < \
+                                        new_guild_data.ranks.index(_member.rank):
+                                    # Demoted
+                                    log.info("Member demoted: " + _member.name)
+                                    changes.append({"type": "demotion", "member": _member})
+                                else:
+                                    # Promoted
+                                    log.info("Member promoted: " + _member.name)
+                                    _member["type"] = "promotion"
+                                    changes.append({"type": "promotion", "member": _member})
+                            except ValueError:
+                                # Todo: Handle this
+                                pass
+                        # Title changed
+                        if member.title != _member.title:
+                            log.info("Member title changed: {0.name} - {0.title}".format(member))
+                            changes.append({"type": "title", "member": _member, "old_title": member.title})
+                        break
+                if not found:
+                    # We check if it was a namechange or character deleted
+                    log.info("Checking character {0.name}".format(member))
+                    char = get_character(member.name)
+                    # Character was deleted (or maybe namelocked)
+                    if char is None:
+                        changes.append({"type": "deleted", "member": member})
+                        continue
+                    # Character has a new name and matches someone in guild, meaning it got a name change
+                    _found = False
+                    for _member in new_guild_data.members:
+                        if char.name == _member.name:
+                            new_guild_data.members.remove(_member)
+                            changes.append({"type": "name_change", "member": _member, "former_name": member.name})
+                            log.info("{former_name} changed name to {name}".format(**_member))
+                            _found = True
+                            break
+                    if _found:
+                        continue
+                    log.info("Member no longer in guild: " + member.name)
+                    changes.append({"type": "removed", "member": member})
+                joined = new_guild_data.members[:]
+                if len(joined) > 0:
+                    log.info("New members found: " + ",".join(m.name for m in joined))
+                if guild["override_image"]:
+                    guild["avatar_url"] = new_guild_data.logo_url
+                announce_changes(guild, name, changes, joined, total_members)
+                log.info(name + " - Scanning done")
+                time.sleep(2)
+            time.sleep(5 * 60)
+
+
 if __name__ == "__main__":
-    main()
+    scan_guilds()
