64eda2cbd9cf2d07b7561915a4fe295d5d8da8f0
Allan Galarza <allan.galarza@gmail.com>
2018-08-12 22:27:04 -0700

    Include Python 3.7

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Communications :: Chat',
         'Topic :: Games/Entertainment',
         'Topic :: Games/Entertainment :: Role-Playing'
